ef52467d0e9fa402bda06bb406317679f267a9c4
Hitesh Suresh <hitesh444appu@gmail.com>
2025-07-01 02:22:01 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,116 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+from collections import deque
+import pyttsx3
+from utilities import *
+
+
+text_voice = pyttsx3.init()
+
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.7,max_num_hands = 1)
+mp_draw = mp.solutions.drawing_utils
+
+
+capture = cv2.VideoCapture(0)
+
+
+pts = deque(maxlen=100)  # max = Last 100 positions of the reference
+
+last_spoken = ""
+
+try:
+
+    while True:
+        ret, frame = capture.read()
+        frame = cv2.flip(frame, 1)
+        if not ret:
+            print("failed to capture...")
+            break
+
+        h, w, _ = frame.shape
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        results = hands.process(frame)
+
+        log = False
+
+
+        if results.multi_hand_landmarks:    
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+
+    
+                x_list = [int(lm.x * w) for lm in hand_landmarks.landmark]
+                y_list = [int(lm.y * h) for lm in hand_landmarks.landmark]
+
+                x_min, x_max = min(x_list), max(x_list)
+                y_min, y_max = min(y_list), max(y_list)
+                cv2.rectangle(frame, (x_min, y_min), (x_max, y_max), (255, 0, 0), 2)
+
+                bounding_box = [x_min, x_max, y_min, y_max]
+
+
+                area = (x_max - x_min) * (y_max - y_min)
+                depth = hand_landmarks.landmark[0].z 
+
+                if log:
+                    loging(area,depth)
+                # print("Bounding Box Area:", area, "depth:" , depth)
+
+                centre_x,centre_y = centre_point(bounding_box)
+
+                pts.append((centre_x, centre_y))
+                cv2.circle(frame, (centre_x, centre_y), 8, (0, 255, 0), 1)
+                
+        experiment_mute = False
+
+        # Gesture direction
+        direction = get_direction(pts)
+        if direction and direction != last_spoken:
+            print(direction)
+            text_voice.say(direction)
+            text_voice.runAndWait()
+
+            if direction == "Volume up":
+                volume_up()
+
+            elif direction == "Volume down":
+                volume_down()
+
+            last_spoken = direction
+            pts.clear()
+
+        if experiment_mute:
+            if 8400 < area < 8500:
+                toggle_mute()
+                text_voice.say("muted")
+                text_voice.runAndWait()
+            elif 49000 < area < 50000:
+                text_voice.say("un muted")
+                text_voice.runAndWait()
+                toggle_mute()
+
+        pose = get_pose(frame, recognizer)
+        if pose == "Fist":
+            toggle_mute()
+            text_voice.say("Muted")
+            text_voice.runAndWait()
+        elif pose == "Open_Palm":
+            toggle_mute()
+            text_voice.say("Unmuted")
+            text_voice.runAndWait()
+
+
+        # Display 
+        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        cv2.imshow("Hand Movement Tracker", frame_bgr)
+        if cv2.waitKey(1) & 0xFF == 27:  
+            break
+
+    capture.release()
+    cv2.destroyAllWindows()
+
+except Exception as e:
+    print("exception", e, type(e))
---
+++ utilities.py
@@ -0,0 +1,65 @@
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+import os,csv
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+model_path = 'D:\Git_projects\Gesture Recognition\gesture_recognizer.task'
+recognizer = vision.GestureRecognizer.create_from_model_path(model_path)
+
+
+
+def centre_point(bounding_box):
+    centre_x = (bounding_box[0] + bounding_box[1]) // 2
+    centre_y = (bounding_box[2] + bounding_box[3]) // 2 
+    return centre_x,centre_y
+
+def get_direction(pts):
+    if len(pts) >= 30:
+        dx = pts[-1][0] - pts[0][0]
+        dy = pts[-1][1] - pts[0][1]
+        if abs(dx) > 50 and abs(dx) > abs(dy):
+            return "Turning Right" if dx > 0 else "Turning Left"
+        elif abs(dy) > 50:
+            return "Volume down" if dy > 0 else "Volume up"
+    return None
+
+def control_volume():
+    devices = AudioUtilities.GetSpeakers()
+    interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+    return cast(interface, POINTER(IAudioEndpointVolume))
+
+volume = control_volume()
+
+
+def volume_up(step=0.1):
+    current = volume.GetMasterVolumeLevelScalar()
+    volume.SetMasterVolumeLevelScalar(min(current + step, 1.0), None)
+
+def volume_down(step=0.1):
+    current = volume.GetMasterVolumeLevelScalar()
+    volume.SetMasterVolumeLevelScalar(max(current-step,0.0),None)
+
+def toggle_mute():
+    muted = volume.GetMute()
+    volume.SetMute(not muted, None)
+
+def get_pose(frame, recognizer):
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=frame)
+    result = recognizer.recognize(mp_image)
+    if result.gestures:
+        return result.gestures[0][0].category_name
+    return None
+
+def loging(area,depth):
+    csv_path = r"D:\Git_projects\Gesture Recognition\log(fist).csv"
+
+    file_exists = os.path.isfile(csv_path)
+    with open(csv_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(["area", "depth"])
+        writer.writerow([area, depth])
+
